b578f03829a679ef90ccfc3c3b872026fa061627
ares <learslane97@gmail.com>
2024-04-02 22:59:33 +0200

    feat: added lazy monad and a simple IOMonad example

---
+++ src/main/scala/monad/IOMonad.scala
@@ -0,0 +1,30 @@
+package monad
+
+import scala.io.StdIn.readLine
+
+object IOMonad {
+
+  def question: Unit = {
+    println("What is your name?")
+    val name = readLine
+    println(f"Hello, $name!")
+  }
+
+  case class IO[A](unsafeRun: () => A) { self =>
+    def flatMap[B](f: A => IO[B]): IO[B] = IO(
+      () => f(self.unsafeRun()).unsafeRun()
+    )
+    def map[B](f: A => B): IO[B] = IO(() => f(self.unsafeRun()))
+  }
+
+  def printString(line: String): IO[Unit] = IO(() => println(line))
+  def readStdIn: IO[String]               = IO(() => readLine)
+
+  def betterQuestion: IO[Unit] =
+    for {
+      _    <- printString("What is your name?")
+      name <- readStdIn
+      _    <- printString(f"Hello, $name!")
+    } yield ()
+
+}
